e110661552a3474d978998daea31365aaa964c68
Renato Yoiti <renatoyoiti@gmail.com>
2020-01-29 11:34:39 -0300

    feat: add Schedule controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -25,6 +26,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
