4ba039935ac6471e3e6838a551cf784ed95ceeab
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-09 10:55:44 -0300

    Initial commit Web Version

---
+++ web/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks',
+    'jsx-ally',
+    'import'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js' ] }
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'import/prefer-default-export': 'off',
+    'react/prefer-stateless-function': 'off',
+    'no-console': ['error', { allow: ['tron']}],
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-console': ['error', { allow: ['tron'] }],
+    camelcase: off,
+  },
+};
