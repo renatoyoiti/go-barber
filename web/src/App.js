f9e93ec681bf0ef14957923132e8d19744b7b4b4
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-09 14:10:20 -0300

    feat: pages layouts & styles

--- web/src/App.js
+++ web/src/App.js
@@ -5,10 +5,13 @@ import history from './services/history';
 import Routes from './routes';
 import './config/ReactotronConfig';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   )
 }
