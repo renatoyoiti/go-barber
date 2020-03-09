2e6959bba02b4bfa820413132f8832d53651717d
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-09 11:10:16 -0300

    refactor: user data returned

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -21,7 +22,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({
@@ -35,13 +45,15 @@ class SessionController {
       });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
