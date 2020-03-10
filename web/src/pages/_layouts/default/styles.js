9c29deaa4098daa44634fa6358195388a35c55cc
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-10 16:24:48 -0300

    feat: add sign in validation

--- web/src/pages/_layouts/default/styles.js
+++ web/src/pages/_layouts/default/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #434343, #000);
+  background: linear-gradient(-90deg, #0f0c29, #302b63, #24243e);
 `;
