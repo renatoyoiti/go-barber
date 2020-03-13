0b870e6bb412c2e0dd81a70ea7240098904e8835
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-13 17:06:32 -0300

    config: add background Component

---
+++ mobile/src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#0f0c29', '#302b63', '#24243e'],
+})`
+  flex: 1;
+`;
