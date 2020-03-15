b3272ef042d5fddf0564af65aa7880917fbffd17
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-15 09:52:34 -0300

    feat: Input & Button components

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input icon="call" placeholder="Digite seu nome" style={{ marginTop: 30 }} />
+      <Button>Entrar</Button>
     </Background>
   )
 }
