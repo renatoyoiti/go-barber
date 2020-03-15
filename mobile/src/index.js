a155911ef24508a31fbfe88c8151a27e1fe41b1f
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-15 11:46:03 -0300

    add redux & redux-saga libs & reactotron config

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+
+import './config/ReactotronConfig';
+
 import { StatusBar } from 'react-native';
 
 import Routes from './routes';
