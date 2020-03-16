73f248ba8bcbf71d2d32811bbfa56f6c72bbcdb0
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-15 22:08:52 -0300

    feat: add react-tab-navigation & routes groups

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -7,14 +7,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
-import Routes from './routes';
+import App from './App';
 
-export default function App() {
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#0f0c29" />
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
